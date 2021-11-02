6aaed2db6bc3b202e0b527a615a743f14f393ac6
Matheus Amendoeira Ferraria <37910467+matheusaf@users.noreply.github.com>
2021-11-02 00:33:10 +0000

    chromedriver

--- src/main/java/com/teste/selenium/App.java
+++ src/main/java/com/teste/selenium/App.java
@@ -1,13 +1,27 @@
 package com.teste.selenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/workspaces/TesteSelenium/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://bulbapedia.bulbagarden.net/wiki/Main_Page");
+        // 2 | setWindowSize | 974x1032 |
+        driver.manage().window().setSize(new Dimension(974, 1032));
+        // 3 | click | linkText=By Name |
+        driver.findElement(By.linkText("By Name")).click();
+        // 4 | click | linkText=Bulbasaur |
+        driver.findElement(By.linkText("Bulbasaur")).click();
+        System.out.println(driver.getTitle());
     }
+
 }
--- src/test/java/com/teste/selenium/AppTest.java
+++ src/test/java/com/teste/selenium/AppTest.java
@@ -1,20 +1,46 @@
 package com.teste.selenium;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
+    private static WebDriver driver;
+
     /**
      * Rigorous Test :-)
      */
+
+    @BeforeAll
+    public static void setUp() {
+        System.setProperty("webdriver.chrome.driver", "/workspaces/TesteSelenium/msedgedriver");
+        driver = new ChromeDriver();
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        // 1 | open | /wiki/Main_Page |
+        driver.get("https://bulbapedia.bulbagarden.net/wiki/Main_Page");
+        // 2 | setWindowSize | 974x1032 |
+        driver.manage().window().setSize(new Dimension(974, 1032));
+        // 3 | click | linkText=By Name |
+        driver.findElement(By.linkText("By Name")).click();
+        // 4 | click | linkText=Bulbasaur |
+        driver.findElement(By.linkText("Bulbasaur")).click();
+        assertTrue(driver.getCurrentUrl() == "ss");
+    }
+
+    @AfterAll
+    public static void destroy() {
+        driver.quit();
     }
 }
